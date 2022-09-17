e0fe47358ed7a6e4d193835078648a2332216d87
Enzo Fagundes <enzo.fagundes014@gmail.com>
2022-09-17 20:26:51 -0300

    home com breakpoints de celualr e tablet

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,4 +1,15 @@
 var swiper = new Swiper('.mySwiper', {
     slidesPerView: 2,
     slidesPerColumn: 1,
-  });
+    breakpoints:{
+      0: {
+          slidesPerView: 2,
+      },
+      1025: {
+          slidesPerView: 4,
+      },
+      1441: {
+          slidesPerView: 4,
+      },
+  },
+});
