d9d36c74cc3240912660a45c3487ac3b240c3a01
Enzo Fagundes <enzo.fagundes014@gmail.com>
2022-10-20 17:49:39 -0300

    popup modal adicionado

---
+++ assets/js/modal.js
@@ -0,0 +1,24 @@
+const BODY = document.querySelector('body');
+let modal = document.querySelector(".modal");
+let closeButton = document.querySelector(".close-button");
+let closeText = document.querySelector(".close-text");
+let flag = false;
+
+BODY.onload = () => {
+    let userString = localStorage.getItem('user');
+
+    if (userString == null) {
+        setTimeout(toggleModal(), 300000);
+    }
+    else
+    {
+        flag = true;
+    }
+}
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+closeButton.addEventListener("click", toggleModal);
+closeText.addEventListener("click", toggleModal);
