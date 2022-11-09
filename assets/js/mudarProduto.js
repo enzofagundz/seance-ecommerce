769c07d69d4bde17a56a87645f6182fdfa8b1576
Enzo Fagundes <enzo.fagundes014@gmail.com>
2022-11-09 05:09:15 -0300

    acabamento da página produto

--- assets/js/mudarProduto.js
+++ assets/js/mudarProduto.js
@@ -1,22 +1,26 @@
 let cadeiraString = sessionStorage.getItem('cadeira');
 let cadeira = JSON.parse(cadeiraString);
 
-let divProdutos = document.querySelector('.recommended-products');
-let imgProdutos = document.querySelector('.prod-img');
-let nomeProdutos = document.querySelector('.recommended-products-title');
-let precoProdutos = document.querySelector('.recommended-products-price');
-let descricaoProdutos = document.querySelector('.recommended-products-description');
+//let divProdutos = document.querySelector('.recommended-products');
+let imgProdutos = document.querySelector('.product-image-container');
+let nomeProdutos = document.querySelector('.product-name');
+let precoProdutos = document.querySelector('.product-price');
+let descricaoProdutos = document.querySelector('.product-description');
+let tituloProduto = document.querySelector('.navbar-title');
 
-let title = document.createTextNode(cadeira.nome); 
+let title = document.createTextNode(cadeira.nome);
+let text = document.createTextNode(cadeira.nome);
 let price = document.createTextNode(cadeira.preco);  
-let description = document.createTextNode(cadeira.descricao);  
+let description = document.createTextNode(cadeira.descricao);
 
 let img = document.createElement('img'); 
-img.classList.add('recommended-products-image');       
+img.classList.add('product-image');       
 img.setAttribute('src', cadeira.img);
-
+img.setAttribute('alt', cadeira.descricao)
 
 nomeProdutos.appendChild(title);
 imgProdutos.appendChild(img);
 precoProdutos.appendChild(price);
 descricaoProdutos.appendChild(description);
+tituloProduto.appendChild(text);
+document.title = cadeira.nome;
--- assets/js/produto.js
+++ assets/js/produto.js
@@ -1,35 +1,59 @@
 let cadeiras = [
-    {nome: "Cadeira branca D'Rossi", 
-    img: 'https://i.postimg.cc/dhHVZxyt/Sem-T-tulo-1-01.jpg',  
-    preco: '35.90', descricao: 'Cadeira de jantar com design moderno e confortável.'},
+    {nome: "Sofá branco D'Rossi", 
+    img: 'https://i.postimg.cc/5YVx3K8g/Sem-T-tulo-1-01.jpg',  
+    preco: '385.90', descricao: 'Sofá branco de dois lugares com dois travesseiros e pés metálicos'},
     
-    {nome: 'Poltrona branca Combinare', 
-    img: 'https://i.postimg.cc/3yzrn01s/Sem-T-tulo-1-02.jpg',
-    preco: '99.90', descricao: 'Cadeira de escritório ergonômica'},
+    {nome: 'Cadeira branca Combinare', 
+    img: 'https://i.postimg.cc/LnXmN1gc/Sem-T-tulo-1-02.jpg',
+    preco: '99.90', descricao: 'Cadeira branca estofada acompanhada de uma almofada com pernas de madeira clara"'},
 
     {nome: 'Cadeira branca Luxo Sued',
-    img: 'https://i.postimg.cc/ZvSbhk1j/Sem-T-tulo-1-03.jpg',
-    preco: '59.90', descricao: 'Cadeira de escritório com design moderno e confortável.'},
+    img: 'https://i.postimg.cc/xqg0vR8G/Sem-T-tulo-1-03.jpg',
+    preco: '59.90', descricao: 'Cadeira branca estofada com pernas de madeira preta e detalhes circulares'},
 
     {nome: 'Cadeira cinza AM Decor',
-    img: 'https://i.postimg.cc/DWSyrPrQ/Sem-T-tulo-1-04.jpg',
+    img: 'https://i.postimg.cc/BLBJw5g0/Sem-T-tulo-1-04.jpg',
     preco: '49.90', descricao: 'Cadeira de escritório com design moderno e confortável.'},
     
-    {nome: 'Cadeira preta Combinare',
-    img: 'https://i.postimg.cc/D4P2vjhh/Sem-T-tulo-1-05.jpg',
-    preco: '19.90', descricao: 'Cadeira de escritório com design moderno e confortável.'},
+    {nome: 'Banco preto Combinare',
+    img: 'https://i.postimg.cc/zHDqmLtR/Sem-T-tulo-1-05.jpg',
+    preco: '19.90', descricao: 'Banco de madeira preta tamanho baixo.'},
 
-    {nome: 'Cadeira marrom Luxo Sued',
-    img: 'https://i.postimg.cc/bDbyMYFs/Sem-T-tulo-1-06.jpg',
-    preco: '59.90', descricao: 'Cadeira de escritório com design moderno e confortável.'},
+    {nome: 'Cadeira branca Séance',
+    img: 'https://i.postimg.cc/QVj8KP8x/Sem-T-tulo-1-06.jpg',
+    preco: '59.90', descricao: 'Cadeira de branca na cor marrom com braços altos e pés de madeira com detalhes circulares'},
 
-    {nome: 'Cadeira marrom Séance',
-    img: 'https://i.postimg.cc/mhYZyDHn/Sem-T-tulo-1-07.jpg',
-    preco: '27.90', descricao: 'Cadeira de escritório com design moderno e confortável.'},
+    {nome: 'Cadeira branca Herman Miller',
+    img: 'https://i.postimg.cc/4mSsMxzm/Sem-T-tulo-1-07.jpg',
+    preco: '229.90', descricao: 'Cadeira de escritório branca com rodinhas e pés metálicos.'},
 
-    {nome: 'Cadeira preta Gran Belo',
-    img: 'https://i.postimg.cc/v4BQytNR/Sem-T-tulo-1-08.jpg',
-    preco: '49.90', descricao: 'Cadeira de escritório com design moderno e confortável.'},
+    {nome: 'Cadeira cinza Gran Belo',
+    img: 'https://i.postimg.cc/k2RqdPcw/Sem-T-tulo-1-08.jpg',
+    preco: '55.90', descricao: 'Cadeira de escritório com design moderno e confortável.'},
+
+    {nome: 'Cadeira Herman Miller',
+    img: 'https://i.postimg.cc/F1CMFn16/Sem-T-tulo-1-09.jpg',
+    preco: '24.90', descricao: 'Cadeira de escritório branca com rodinhas e pés metálicos.'},
+
+    {nome: 'Cadeira Herman Miller',
+    img: 'https://i.postimg.cc/SJXwn7Mx/Sem-T-tulo-1-10.jpg',
+    preco: '24.90', descricao: 'Cadeira de escritório branca com rodinhas e pés metálicos.'},
+
+    {nome: 'Cadeira Herman Miller',
+    img: 'https://i.postimg.cc/06v3XTHz/Sem-T-tulo-1-11.jpg',
+    preco: '24.90', descricao: 'Cadeira de escritório branca com rodinhas e pés metálicos.'},
+
+    {nome: 'Cadeira Herman Miller',
+    img: 'https://i.postimg.cc/zyK6jzpm/Sem-T-tulo-1-12.jpg',
+    preco: '24.90', descricao: 'Cadeira de escritório branca com rodinhas e pés metálicos.'},
+
+    {nome: 'Cadeira Herman Miller',
+    img: 'https://i.postimg.cc/dZhSTwDx/Sem-T-tulo-1-13.jpg',
+    preco: '24.90', descricao: 'Cadeira de escritório branca com rodinhas e pés metálicos.'},
+
+    {nome: 'Cadeira Herman Miller',
+    img: 'https://i.postimg.cc/jnnZScFV/Sem-T-tulo-1-14.jpg',
+    preco: '24.90', descricao: 'Cadeira de escritório branca com rodinhas e pés metálicos.'}
 ];
 
 function produto (id) {
@@ -37,6 +61,6 @@ function produto (id) {
         console.log(id);
 
         sessionStorage.setItem('cadeira', JSON.stringify(cadeiras[id]));
-        sessionStorage.setItem('recommended-products', id);
+        //sessionStorage.setItem('recommended-products', id);
         window.location = '/assets/pages/produto.html';
 }
