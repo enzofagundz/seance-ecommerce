12bb3ea6d7c97978b2320b71b6af3372b293d348
Enzo Fagundes <88722351+enzofagundz@users.noreply.github.com>
2022-11-28 10:01:10 -0300

    Add files via upload

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,10 +11,22 @@ let user = {
 let userString = localStorage.getItem('user');
 let userObject = JSON.parse(userString);
 
+let numbersCaptcha = [];
+numbersCaptcha[0] = Math.floor(Math.random() * 20);
+numbersCaptcha[1] = Math.floor(Math.random() * 20);
+
+let captchaLabel = document.querySelector('#label-resultado');
+let resultadoCaptcha = numbersCaptcha[0] + numbersCaptcha[1];
+let captchaText = document.createTextNode(`Quanto é ${numbersCaptcha[0]} + ${numbersCaptcha[1]}?`);
+
+captchaLabel.appendChild(captchaText);
+
 BUTTON.addEventListener('click', function(event){
     event.preventDefault();
 
     document.querySelector('.form-answer-login-text').textContent = ' ';
+
+    let captchaInput = document.querySelector('#resultado').value;
 
     user.mail = document.querySelector('#email').value;
     user.password = document.querySelector('#password').value;
@@ -28,6 +40,10 @@ BUTTON.addEventListener('click', function(event){
         {
             text = document.createTextNode('Campos e-mail vazio');
         }
+        else if (resultadoCaptcha == '')
+        {
+            text = document.createTextNode('Insira a resposta para o Captcha')
+        }
         else
         {
             text = document.createTextNode('Campo senha vazio');
@@ -35,13 +51,19 @@ BUTTON.addEventListener('click', function(event){
     }
     else
     {
-        if (user.password == userObject.password && userObject.password == user.password)
-        {
-            window.location = '/index.html';
+        if (resultadoCaptcha == captchaInput) {
+            if (user.password == userObject.password && userObject.password == user.password)
+            {
+                window.location = '/index.html';
+            }
+            else
+            {
+                text = document.createTextNode('A senha está incorreta');
+            }
         }
         else
         {
-            text = document.createTextNode('A senha está incorreta');
+                text = document.createTextNode('A resposta do captcha está incorreta');
         }
     }
     
--- assets/js/redefinir.js
+++ assets/js/redefinir.js
@@ -11,7 +11,17 @@ let user = {
 let text;
 let textArea = document.querySelector('.form-answer-text');
 
-BUTTON.addEventListener('click', function(event){
+let numbersCaptcha = [];
+numbersCaptcha[0] = Math.floor(Math.random() * 20);
+numbersCaptcha[1] = Math.floor(Math.random() * 20);
+
+let captchaLabel = document.querySelector('#label-resultado');
+let resultadoCaptcha = numbersCaptcha[0] + numbersCaptcha[1];
+let captchaText = document.createTextNode(`Quanto é ${numbersCaptcha[0]} + ${numbersCaptcha[1]}?`);
+
+captchaLabel.appendChild(captchaText);
+
+BUTTON.addEventListener('click', function (event) {
     event.preventDefault();
 
     document.querySelector('.form-answer-text').textContent = ' ';
@@ -19,53 +29,50 @@ BUTTON.addEventListener('click', function(event){
     user.mail = document.querySelector('#email').value;
     user.password = document.querySelector('#password').value;
     user.newPassword = document.querySelector('#newPassword').value;
-
-    if (user.mail == '' || user.password == '' || user.newPassword == '')
-    {
-        if (user.mail == '' && user.password == '' && user.newPassword == '')
-        {
+    let captchaInput = document.querySelector('#resultado').value;
+    
+    if (user.mail == '' || user.password == '' || user.newPassword == '') {
+        if (user.mail == '' && user.password == '' && user.newPassword == '') {
             text = document.createTextNode('Campos vazios, insira as informações');
         }
-        else if (user.mail== '')
-        {
+        else if (user.mail == '') {
             text = document.createTextNode('Campos e-mail vazio');
         }
-        else if (user.password == '')
-        {
+        else if (user.password == '') {
             text = document.createTextNode('Campo senha vazio');
         }
-        else
-        {
+        else if (resultadoCaptcha == '') {
+            text = document.createTextNode('Insira a resposta para o Captcha')
+        }
+        else {
             text = document.createTextNode('Campo para confirmar senha vazio');
         }
     }
-    else
-    {
-        if (userObject.mail == user.mail)
-        {
-            if (user.password == userObject.password)
-            {
-            text = document.createTextNode('Esta é sua senha atual, insira uma nova senha');
+    else {
+        if (userObject.mail == user.mail) {
+            if (user.password == userObject.password) {
+                text = document.createTextNode('Esta é sua senha atual, insira uma nova senha');
             }
-            else
-            {
-                if (user.password == user.newPassword)
-                {
-                    userObject.password = user.newPassword;
-                    localStorage.setItem('user', JSON.stringify(user));
-                    window.location = '/index.html';
+            else {
+                if (resultadoCaptcha == captchaInput) {
+                    if (user.password == user.newPassword) {
+                        userObject.password = user.newPassword;
+                        localStorage.setItem('user', JSON.stringify(user));
+                        window.location = '/index.html';
+                    }
+                    else {
+                        text = document.createTextNode('As senhas não coincidem');
+                    }
                 }
-                else
-                {
-                    text = document.createTextNode('As senhas não coincidem');
+                else {
+                    text = document.createTextNode('A resposta do captcha está incorreta');
                 }
             }
         }
-        else
-        {
+        else {
             text = document.createTextNode('O e-mail não corresponde ao cadastrado');
         }
     }
-    
+
     textArea.appendChild(text);
 });
